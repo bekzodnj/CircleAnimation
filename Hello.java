c0b63f247e3a3468e80130ac0e3a7e0ae9b146ef
Bekzodjon Norkuziev <34368503+BekzodjonN@users.noreply.github.com>
2017-12-08 13:51:59 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+* Program shows JavaFX Animation Sample
+*
+* Author @BekzodjonN 
  */
 package hello;
 
